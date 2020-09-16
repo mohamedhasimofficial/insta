f31301c955a5471f0fd961166fd1508de1551f6c
mohamedhasimofficial <71386673+mohamedhasimofficial@users.noreply.github.com>
2020-09-16 19:33:08 +0530

    Add files via upload

---
+++ mohamed-hasim-id-to-insta-convertion.js
@@ -0,0 +1,16 @@
+function myWhatsapp(){
+  var whatsapp,whatsapp_number; 
+  whatsapp = document.getElementById('whatsapp').value;
+  whatsapp_number = "https://instagram.com/"+whatsapp;
+  document.getElementById('number').innerHTML="<b>CLICK TO START USE</b>";
+}
+
+        function myWhatsapp1(){
+          var whatsapp,whatsapp_number; 
+          whatsapp = document.getElementById('whatsapp').value;
+          whatsapp_number = "https://instagram.com/"+whatsapp;
+          document.getElementById('number').innerHTML="<b>CLICK TO START USE</b>";
+          window.open(whatsapp_number);
+        }
+    
+
